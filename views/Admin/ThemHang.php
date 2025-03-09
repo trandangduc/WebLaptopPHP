6c4ddfdf59e9d6d8425f152f4c26eab1319e6cf6
Son <phamvanson2k3@gmail.com>
2025-03-09 15:09:54 +0700

    Admin

---
+++ views/Admin/ThemHang.php
@@ -0,0 +1,25 @@
+﻿@model TMDTLaptop.Models.HangSanPham
+
+@{
+    ViewBag.Title = "Thêm Hãng Sản Phẩm";
+    Layout = "~/Views/Shared/_LayoutAdmin.cshtml";
+}
+
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+            <h2 class="mt-4">@ViewBag.Title</h2>
+
+            @using (Html.BeginForm("ThemHang", "Admin", FormMethod.Post))
+            {
+                <div class="form-group">
+                    <label for="TenHang">Tên Hãng</label>
+                    @Html.TextBoxFor(m => m.TenHang, new { @class = "form-control", placeholder = "Nhập tên hãng" })
+                </div>
+
+                <button type="submit" class="btn btn-success">Lưu</button>
+                <a href="@Url.Action("QuanLyHang")" class="btn btn-secondary">Quay lại</a>
+            }
+        </div>
+    </div>
+</div>
